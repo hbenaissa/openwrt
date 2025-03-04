58bb12000cf668e0563d1001e29266aa0b98c0e4
hbenaissa <hassen.benaissa-ext@ldc.com>
2025-03-04 16:05:10 +0100

    add .config

---
+++ env.sh
@@ -0,0 +1,6 @@
+PATH="/opt/homebrew/opt/make/libexec/gnubin:$PATH"
+PATH="/opt/homebrew/opt/gnu-getopt/bin:$PATH"
+PATH="/opt/homebrew/opt/gettext/bin:$PATH"
+PATH="/opt/homebrew/opt/coreutils/libexec/gnubin:$PATH"
+PATH="/opt/homebrew/opt/findutils/libexec/gnubin:$PATH"
+export PATH
